3a54830e27975b1cf755eb211532a65e8c0a7b0b
Pavel9172 <pavlo_k@mail.ru>
2022-06-15 14:30:07 +0500

    nev test code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,17 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxnewCodeCheckValue() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 40, 6, 11, 11, 12};
+        long expected = 40;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
